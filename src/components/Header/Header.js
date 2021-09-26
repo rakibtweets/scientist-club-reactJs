b71abfaabc51de67e9d3126bc21526f7a1f5a91d
rakibtweets <rakibhasanroky0@gmail.com>
2021-09-26 21:29:34 +0600

    UI designed completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="header__container text-center text-white">
       <h1 className="fw-bold">Greatest Scientist Club</h1>
       <p className="fw-bold">
-        World best scientist is in only club they gonna discover the known
+        World best scientist in one club they gonna discover the known
       </p>
       <h2 className="fw-bold">Budget: 1 Billion</h2>
     </div>
--- src/components/Scientist/Scientist.js
+++ src/components/Scientist/Scientist.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebookSquare,
+  faTwitterSquare,
+} from '@fortawesome/free-brands-svg-icons';
 import './Scientist.css';
 
 const Scientist = (props) => {
   const { name, age, role, salary, country, image, famousInvention, lifeSpan } =
     props.scientist;
   const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />;
+  const facebook = <FontAwesomeIcon icon={faFacebookSquare} />;
+  const twitter = <FontAwesomeIcon icon={faTwitterSquare} />;
   return (
-    <div className="scientist col-4">
+    <div className="scientist col-4 mb-3">
       <div className="card__container card">
         <div className="d-flex justify-content-center p-2">
           <img className="card__img" src={image} alt="scientist-img" />
         </div>
-        <div className="card-body text-start ps-3">
+        <div className="card-body text-start ps-4">
           <h5 className="text-center fw-bold">{name}</h5>
-          <h6 className="text-center fw-bold">
+          <h6 className="text-center fw-bold pb-3">
             <small>{lifeSpan}</small>
           </h6>
           <p className="m-0 pb-1">
@@ -35,14 +41,22 @@ const Scientist = (props) => {
             <strong>Salary:</strong> $ {salary}
           </p>
         </div>
-        <div className="text-center pb-4">
+        <div className="text-center mb-1">
           <button
             onClick={() => props.handleAddtoCart(props.scientist)}
             href="#0"
-            className="btn btn-primary mb-auto"
+            className="btn btn-primary"
           >
             {cartIcon} add to cart
           </button>
+        </div>
+        <div className="media__icon mb-3">
+          <a className="icon" href="#0">
+            {facebook}
+          </a>
+          <a className="icon" href="#0">
+            {twitter}
+          </a>
         </div>
       </div>
     </div>
